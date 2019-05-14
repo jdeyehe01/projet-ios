18d992078f29c7e0fb20d369426991bc9e2d4c3b
etudiant <etudiant@iMac-de-etudiant.local>
2019-05-14 17:04:49 +0200

    first commit

---
+++ Football App/Football App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Football App
+//
+//  Created by etudiant on 14/05/2019.
+//  Copyright © 2019 etudiant. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
